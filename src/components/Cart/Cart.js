26516c16477242755e60786dd03cdb8065f8b2d3
Payal Sasmal <sasmalpayal@gmail.com>
2022-12-07 21:44:15 +0530

    Cart is added

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <div>Total Amount</div>
+        <div>35.62</div>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+export default Cart;
