2eb9a79e7d032641ccf082e45673d089a2596c5e
Payal Sasmal <sasmalpayal@gmail.com>
2022-12-11 21:57:51 +0530

    adding a cart reducer

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,29 @@
+import { useRef } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
+
 const MealItemForm = (props) => {
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={classes.form}>
-      <Input label="Amount" input={{ 
-        id: "amount_" + props.id,
-        type: "number",
-        min: '1',
-        max: '5',
-        step: '1',
-        defaultValue: '1'
-        }} />
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,9 +1,10 @@
 import classes from './Input.module.css';
 
-const Input = props => {
-    return <div className={classes.input}>
+const Input = React.forwardRef((props, ref) => {
+    return ( <div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>
+        <input ref={ref} {...props.input}/>
     </div>
-};
+    );
+});
 export default Input;
